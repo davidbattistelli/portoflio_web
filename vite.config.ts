3b959b56f956e2245318b3ef467869c675bbf4d5
David Battistelli <david.battistelli@studenti.unitus.it>
2025-11-06 18:25:06 +0100

    Update base path for renamed repository (my_portfolio)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portoflio_web/', // GitHub Pages base path
+  base: '/my_portfolio/', // GitHub Pages base path
 })
 
